54f03576abb5bb7ba2792e4fa2b2b7095e7e11e7
Joon Park <52639657+JoonyBoy4713@users.noreply.github.com>
2019-07-10 11:31:14 +1000

    Update output.intro.py

--- Homework/output.intro.py
+++ Homework/output.intro.py
@@ -4,6 +4,6 @@ job = "tennis player"
 year = 2018
 title_Roland_Garros = 11
 
-print(name + "is" + job)
-print("he is" + nickname)
-print ("he has won a record" + title_Roland_Garros + (int("at French open until" + year)
+print(name + "is" + job + ".")
+print("he is" + nickname + ".")
+print("he has won a record" + str(title_Roland_Garros) + "at French open until" + str(year) + (".")
