a93f141b63e5144b2d6cae1d0de1f225883a6533
Joon Park <52639657+JoonyBoy4713@users.noreply.github.com>
2019-07-10 11:12:15 +1000

    Update output.intro.py

--- Homework/output.intro.py
+++ Homework/output.intro.py
@@ -6,4 +6,4 @@ title_Roland_Garros = 11
 
 print(name + "is" + job)
 print("he is" + nickname)
-print ("he has won a record" + title_Roland_Garros + "at French open until" + year)
+print ("he has won a record" + title_Roland_Garros + (int("at French open until" + year)
