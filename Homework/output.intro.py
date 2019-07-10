e70030d3d63197f2bb69d1633919a36f25f31bf4
Joon Park <52639657+JoonyBoy4713@users.noreply.github.com>
2019-07-10 11:00:28 +1000

    Create output.intro.py

---
+++ Homework/output.intro.py
@@ -0,0 +1,9 @@
+name = "Rafael Nadal"
+nickname = "the king of clay"
+job = "tennis player"
+year = 2018
+title_Roland_Garros = 11
+
+print(name + "is" + job)
+print("he is" + nickname)
+print ("he has won a record" + title_Roland_Garros + "at French open until" + year)
