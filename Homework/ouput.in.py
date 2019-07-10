d126f80601f6c685532ce6845c0c88e5a46bd9de
Joon Park <52639657+JoonyBoy4713@users.noreply.github.com>
2019-07-10 21:35:20 +1000

    Create ouput.in.py

---
+++ Homework/ouput.in.py
@@ -0,0 +1,13 @@
+name = "Rafael Nadal"
+nickname = "the king of clay"
+job = "tennis player"
+year = 2018
+title_Roland_Garros = 11
+
+sentence1 = name + "is" + job + "."
+sentence2 = "he is" + nickname + "."
+sentence3 = "he has won a record" + str(title_Roland_Garros) + "at French open until" + str(year) + (".")
+
+print(sentence1)
+print(sentence2)
+print(sentence3)
