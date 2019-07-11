ee39a49772e693404656b189016d84cf4e5a2e5a
Joon Park <52639657+JoonyBoy4713@users.noreply.github.com>
2019-07-11 22:45:35 +1000

    fff

---
+++ 41-intro-funtion.py
@@ -0,0 +1,7 @@
+def add(num1,num2)
+   print(f"{num1} + {num2} = {num1 + num2}")
+
+add(3, 5)
+add(2,4)
+add(5, 7)
+add(9, 7)
---
+++ Chart.Output formatting.py
@@ -0,0 +1,31 @@
+field1 = "Name"
+field2 = "Age"
+
+name1 = "Sunil Dermott"
+name2 = "Carson Van Der Beek"
+name3 = "Putu Anand"
+name4 = "Vergilius Campana"
+name5 = "Marijona Jansing"
+
+age1 = 105
+age2 = 8
+age3 = 23
+age4 = 9
+age5 = 36
+define = "________________________"
+#print(field1)
+#print(field2)
+#print(user_line_1)
+#print(user_line_2)
+#print(user_line_3)
+#print(user_line_4)s
+#print(user_line_5)
+
+print(f"Name                  Age")
+print(f"{define}")
+print(f"{name1}         :{age1}")
+print(f"{name2}   :{age2}")
+print(f"{name3}            :{age3}")
+print(f"{name4}     :{age4}")
+print(f"{name5}      :{age5}")
+
